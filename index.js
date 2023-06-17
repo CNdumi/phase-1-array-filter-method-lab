c121962c62e95021495a2dd384d2680e6f652d02
CNdumi <carolinendumi12@gmail.com>
2023-06-17 14:08:36 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Code your solution here
+function findMatching(array, toFind){
+    let returnArray = [];
+    for (let i=0;i<array.length;i++){
+        
+        if (array[i].toLowerCase() === toFind.toLowerCase()){
+            returnArray.push(array[i]);
+        }
+    }
+    return returnArray;
+
+}
+
+
+function fuzzyMatch(array, chars){
+    let returnArray = [];
+    for (const val of array){
+        
+        if (val.startsWith(chars)){
+            returnArray.push(val);
+        }
+    }
+    return returnArray;
+}
+
+function matchName(array, toFind){
+    let returnArray = [];
+    for (const val of array){    
+        if (val.name===toFind){
+            returnArray.push(val);
+        }
+    }
+    return returnArray;
+}
+
+const drivers = [
+        {
+          name: 'Bobby',
+          hometown: 'Pittsburgh' },
+        {
+          name: 'Sammy',
+          hometown: 'New York' } ,
+        {
+          name: 'Sally',
+          hometown: 'Cleveland' },
+        {
+          name: 'Annette',
+          hometown: 'Los Angeles' },
+        {
+          name: 'Bobby',
+          hometown: 'Tampa Bay' }
+      ];
+console.log(matchName(drivers, 'Bobby'));
